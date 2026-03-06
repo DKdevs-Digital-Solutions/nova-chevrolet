5b9178cf90ac6c381297228518ac2a2388f8b8a4
Dronasci <122463183+intalkconnect@users.noreply.github.com>
2026-03-06 20:27:32 -0300

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'oficina-v1';
-const PRECACHE = ['/', '/manifest.json'];
+const CACHE = 'nova-chevrolet-v2';
+const PRECACHE = ['/', '/manifest.webmanifest', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(PRECACHE)));
